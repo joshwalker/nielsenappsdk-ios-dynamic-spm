d3751c0880ea2c138ef52ba96395296551849a84
Josh Walker <josh.walker@buzzfeed.com>
2022-07-14 14:42:23 -0500

    removed unused frameworks to prevent build errors

--- Package.swift
+++ Package.swift
@@ -8,31 +8,11 @@ let package = Package(
     products: [
         .library(
             name: "NielsenAppApi",
-            targets: ["NielsenAppApi"]),
-        .library(
-            name: "NielsenNoAdAppApi",
-            targets: ["NielsenNoAdAppApi"]),
-        .library(
-            name: "NielsenNoIdAppApi",
-            targets: ["NielsenNoIdAppApi"]),
-        .library(
-            name: "NielsenAGFAppApi",
-            targets: ["NielsenAGFAppApi"]),
-        .library(
-            name: "NielsenAGFNoAdAppApi",
-            targets: ["NielsenAGFNoAdAppApi"]),
-        .library(
-            name: "NielsenAGFNoIdAppApi",
-            targets: ["NielsenAGFNoIdAppApi"])
+            targets: ["NielsenAppApi"])
     ],
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "NielsenAppApi", path: "products/Global/NielsenAppApi.xcframework"),
-        .binaryTarget(name: "NielsenNoAdAppApi", path: "products/GlobalNoAd/NielsenNoAdAppApi.xcframework"),
-        .binaryTarget(name: "NielsenNoIdAppApi", path: "products/GlobalNoId/NielsenNoIdAppApi.xcframework"),
-        .binaryTarget(name: "NielsenAGFAppApi", path: "products/AGF/NielsenAGFAppApi.xcframework"),
-        .binaryTarget(name: "NielsenAGFNoAdAppApi", path: "products/AGFNoAd/NielsenAGFNoAdAppApi.xcframework"),
-        .binaryTarget(name: "NielsenAGFNoIdAppApi", path: "products/AGFNoId/NielsenAGFNoIdAppApi.xcframework")
+        .binaryTarget(name: "NielsenAppApi", path: "products/Global/NielsenAppApi.xcframework")
     ]
 )
